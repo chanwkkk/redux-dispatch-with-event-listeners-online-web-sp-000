cc2b7243b608aa22922cdd882dd06c1e6905fe8a
Yingqi Chen <chanwk18@hotmail.com>
2020-02-25 18:19:52 +0000

    Automatically backed up by Learn

--- js/reducer.js
+++ js/reducer.js
@@ -1,14 +1,6 @@
 // add code snippets from README
-let state = {count: 0}
 
-function reducer(state, action){
-  switch (action.type) {
-    case 'INCREASE_COUNT':
-      return {count: state.count + 1};
-    default:
-      return state;
-  }
-}
+
 
 function dispatch(action){
   state = reducer(state, action);
@@ -17,4 +9,14 @@ function dispatch(action){
 function render(){
   let container = document.getElementById('container');
   container.textContent = state.count;
+}
+
+let state;
+function reducer(state = {count: 0}, action){
+  switch (action.type) {
+    case 'INCREASE_COUNT':
+      return {count: state.count + 1}
+    default:
+      return state;
+  }
 }
