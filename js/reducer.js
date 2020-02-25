774832dade79d42359ccb477e6a4e3021edcb29f
Yingqi Chen <chanwk18@hotmail.com>
2020-02-25 18:21:36 +0000

    Automatically backed up by Learn

--- js/reducer.js
+++ js/reducer.js
@@ -25,3 +25,10 @@ function render(){
 }
 
 dispatch({type: '@@INIT'})
+
+
+let button = document.getElementById('button');
+ 
+button.addEventListener('click', () => {
+  dispatch({type: 'INCREASE_COUNT'})
+})
